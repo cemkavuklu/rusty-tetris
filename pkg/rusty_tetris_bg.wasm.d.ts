d4e187d5fcfed04036d011b77bcc1e1381df69a8
Cem Kavuklu <cemkavuklu@gmail.com>
2022-07-17 03:14:13 +0300

    Build the game package

---
+++ pkg/rusty_tetris_bg.wasm.d.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function App(a: number, b: number): void;
+export function __wbg___wasmreact_refcontainervalue_free(a: number): void;
+export function wasmreact_useReact(a: number): void;
+export function __wbg_wasmreact_free(a: number): void;
+export function __wbg___wasmreact_componentwrapper_free(a: number): void;
+export function __wasmreact_componentwrapper_render(a: number): number;
+export function __wasmreact_memocomponentwrapper_eq(a: number, b: number): number;
+export function __wbg___wasmreact_memocomponentwrapper_free(a: number): void;
+export function __wasmreact_memocomponentwrapper_render(a: number): number;
+export const __wbindgen_export_0: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc01938b83a80c4ca(a: number, b: number, c: number): number;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h99fce467848b74f1(a: number, b: number): number;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hfaa080af62e65f86(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h000dcd4f2992ac37(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke1_mut_ref__h1ac5c2c276e05cbe(a: number, b: number, c: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
